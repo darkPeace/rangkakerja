99f551e7c138ed3bfc2b5b415a03885a2f530cbd
Hafiz Abu Tahir <notmesh@outlook.com>
2017-03-18 16:12:30 +0800

    tukr cara di kepala

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,8 +1,8 @@
 angular.module('header-module',[])
 
 .controller('header-controller', 
-	['$scope', '$location',
-		function (vm, route) {
+	['$scope', '$location', '$timeout',
+		function (vm, route, timo) {
 
 	vm.brand = {
 		name: 'abs',
@@ -10,16 +10,34 @@ angular.module('header-module',[])
 	};
 	vm.menulist = [{
 		name: 'about',
-		url: '/about'
+		url: '/about',
+		subm: false
 	},{
 		name: 'background',
-		url: '/background'
+		subm: true,
+		subs: [{
+			name: 'sub1',
+			sref: 'home.signin'
+		},{
+			name: 'sub5667',
+			sref: 'home.signin'
+		}]
 	},{
 		name: 'tools',
-		url: '/tools'
+		url: '/tools',
+		subm: false
 	}];
 
 	vm.locate = function (arg) {
 		route.path(arg);
+	}
+
+	vm.openpg = function (brg, arg) {
+		if (arg === undefined) {
+			brg.open();
+		}else{
+			brg.close();
+			vm.locate(arg);
+		}
 	}
 }]);
