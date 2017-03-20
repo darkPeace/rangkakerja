00b6c27a7cbbb20159a9ff3942a19e7a5510a9c9
Hafiz Abu Tahir <notmesh@outlook.com>
2017-03-20 17:58:33 +0800

    unfinished business

--- src/body/pieces/numpad/numpad.js
+++ src/body/pieces/numpad/numpad.js
@@ -17,10 +17,34 @@ function (vm) {
 
 	vm.ops = function (arg) { /* 4-devide 3-multiply 2-minus 1-add */
 		
-		var str1 = parseFloat(vm.display);
+		var str1 = 0;
+		var str2 = parseFloat(vm.display);
 		vm.display = '0';
 		console.log(str1);
+
+		switch(arg){
+			case 4:
+				str1/=str2;
+		console.log(str1);
+				break;
+			case 3:
+				str1*=str2;
+		console.log(str1);
+				break;
+			case 2:
+				str1-=str2;
+		console.log(str1);
+				break;
+			case 1:
+				str1+=str2;
+		console.log(str1);
+				break;
+			case 0:
+				vm.display = str1;
+		console.log(str1);
+				break;
+			default:
+				return false;
+		}
 	}
-
-
 }]);
