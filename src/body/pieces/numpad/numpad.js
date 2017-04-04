45a0908fac687ce06bc6cba05980de0738b30965
Hafiz Abu Tahir <notmesh@outlook.com>
2017-04-04 12:12:45 +0800

    rename calculator to numpad module

--- src/body/pieces/numpad/numpad.js
+++ src/body/pieces/numpad/numpad.js
@@ -2,7 +2,11 @@ angular.module('numpad-module',[])
 .controller('numpad-controller', 
 ['$scope',
 function (vm) {
+
 	vm.display='0';
+	vm.total = null;
+	var str1 = null;
+
 
 	vm.num=function(nbr){
 		if (vm.display !== '0') {
@@ -11,40 +15,46 @@ function (vm) {
 			vm.display=nbr;
 		}
 	}
+
 	vm.clrent = function () {
 		vm.display='0';
 	}
 
+	var c = 0;
+
 	vm.ops = function (arg) { /* 4-devide 3-multiply 2-minus 1-add */
-		
-		var str1 = 0;
-		var str2 = parseFloat(vm.display);
+
+		var s1 = null;
+		var s2 = null;
+
+		if (c===0) {
+			s1 = parseFloat(vm.display);
+			vm.display = '0';
+		} else {
+			s1 = vm.total;
+		}
+
+		s2 = parseFloat(vm.display);
 		vm.display = '0';
-		console.log(str1);
 
 		switch(arg){
 			case 4:
-				str1/=str2;
-		console.log(str1);
+				vm.total = s1 / s2;
 				break;
 			case 3:
-				str1*=str2;
-		console.log(str1);
+				vm.total = s1 * s2;
 				break;
 			case 2:
-				str1-=str2;
-		console.log(str1);
+				vm.total = s1 - s2;
 				break;
 			case 1:
-				str1+=str2;
-		console.log(str1);
+				vm.total = s1 + s2;
 				break;
 			case 0:
-				vm.display = str1;
-		console.log(str1);
-				break;
-			default:
-				return false;
+				vm.display = vm.total;
+				break;		
 		}
+
+		console.log(vm.total);
 	}
 }]);
