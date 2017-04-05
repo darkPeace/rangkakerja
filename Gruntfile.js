44a0eae6aacf83912d56db025b01f36163149c20
Hafiz Abu Tahir <notmesh@outlook.com>
2017-04-05 20:37:03 +0800

    reconfig grunt task setup and file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,197 +1,432 @@
-"use strict";
 
-// grunt wrapper
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-	/* grunt JIT Compiler remove unnecessaey grunt.loadNpmTasks */
-	require('jit-grunt')(grunt); 
-	// need something to rewrite html [src] before publishing /* NOTE */
-	// grunt config
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
 
-		uglify: {
-			test: {
-				files: {
-					'src/min/a.min.js': ['src/**/*.js']
-				}
-			}
-		},
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-		cssmin: {
-			test: {
-				files: {
-					'src/min/a.min.css': ['src/**/*.css']
-				}
-			}
-		},
+  // Automatically load required Grunt tasks
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin'
+  });
 
-		imagemin: {
-			img: {
-				files: [{
-					expand: true,
-					cwd: 'src',
-					src: ['**/*.{png,jpg,gif}'],
-					dest: 'rel'
-				}]
-			}
-		},
+  var serveStatic = require('serve-static');
 
-		htmlmin: {
-			html: {
-				options: {
-					html5: true,
-					removeComments: true,
-					removeEmptyElements: true,
-					collapseWhitespace: true
-				},
+  // Configurable paths for the application
+  var appConfig = {
+    app: require('./bower.json').appPath || 'app',
+    dist: 'dist'
+  };
 
-				files: [{
-					expand: true,
-					cwd: 'src',
-					src: ['**/*.html'],
-					dest: 'rel',
-				}],
-			},
-		},
+  // Define the configuration for all the tasks
+  grunt.initConfig({
 
-		clean: {
-			js: ['rel/js/*.js'],
-			html: ['rel/**/*.html'],
-			css: ['rel/css/*.css'],
-			img: ['rel/img/*.{jpg,png,gif}']
-		},
+    // Project settings
+    apis: appConfig,
 
-		wiredep: {
-			task: {
-				src: [
-					'index.html'
-				],
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+      js: {
+        files: ['<%= apis.app %>/**/*.js'],
+        tasks: ['newer:jshint:all', 'newer:jscs:all'],
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      jsTest: {
+        files: ['test/spec/**/*.js'],
+        tasks: ['newer:jshint:test', 'newer:jscs:test']
+      },
+      styles: {
+        files: ['<%= apis.app %>/styles/**/*.css'],
+        tasks: ['newer:copy:styles', 'postcss']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= apis.app %>/**/*.html',
+          '.tmp/styles/**/*.css',
+          '<%= apis.app %>/imgs/**/*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
 
-				options: {}
-			}
-		},
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          middleware: function (connect) {
+            return [ 
+              serveStatic('.tmp'), 
+              connect().use(
+                '/libs',
+                serveStatic('./libs')
+              ),
+              connect().use(
+                '/app/styles',
+                serveStatic('./app/styles')
+              ),
+              serveStatic(appConfig.app)
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          middleware: function (connect) {
+            return [
+              serveStatic('.tmp'),
+              serveStatic('test'),
+              connect().use(
+                '/libs',
+                serveStatic('./libs')
+              ),
+              serveStatic(appConfig.app)
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          open: true,
+          base: '<%= apis.dist %>'
+        }
+      }
+    },
 
-		connect: {
-			options: {
-				port: 9050,
-				protocol: 'http',
-				hostname: 'localhost',
-				livereload: 38357,
-				// keepalive: true
-			},
-			test:{
-				options: {
-					base: '',
-					open: {
-						app: 'Firefox'
-					}
-				},
-			}
-		},
+    // Make sure there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          // '<%= apis.app %>/**/*.js'
+        ]
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/spec/**/*.js']
+      }
+    },
 
-		copy: {
-			htm: {
-				files:[{
-					expand:false,
-					src: 'src/index.html',
-					dest: 'index.html'
-				}]
-			},
-			main: {
-				files: [{
-					expand: true,
-					src: 'src/index.html',
-					dest: 'rel/index.html'
-				},{
-					expand: true,
-					src: 'src/min',
-					dest: 'rel/min'
-				}]
-			}
-		},
+    // Make sure code styles are up to par
+    jscs: {
+      options: {
+        config: '.jscsrc'
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          '<%= apis.app %>/**/*.js'
+        ]
+      },
+      test: {
+        src: ['test/spec/**/*.js']
+      }
+    },
 
-		watch:{
-			bower: {
-				files: ['bower.json'],
-				tasks: ['wiredep']
-			},
-			grunT: {
-				files: ['Gruntfile.js'],
-				options: {
-					livereload: '<%= connect.options.livereload %>'
-				}
-			},
-			html: {
-				files: ['src/**/*.html'],
-				tasks: ['copy:htm', 'wiredep'],
-				options: {
-					livereload: '<%= connect.options.livereload %>'
-				}
-			},
-			js: {
-				files: ['src/**/*.js', '!src/min/*.min.js'],
-				tasks: [''],
-				options: {
-					livereload: '<%= connect.options.livereload %>'
-				}
-			},
-			css: {
-				files: ['src/**/*.css', '!src/min/*.min.css'],
-				tasks: [''],
-				options: {
-					livereload: '<%= connect.options.livereload %>'
-				}
-			},
-		},
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= apis.dist %>/{,*/}*',
+            '!<%= apis.dist %>/.git{,*/}*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
 
-		jshint: {
-			options: {
-				reporter: require('jshint-stylish'),
-			},
-			raw: {
-				options: {
-					ignores: ['src/min/a.min.js'],
-				},
-				files: {
-					src:['src/**/*.js']
-				},
-			},
-			min: ['src/min/a.min.js'],
-			rel: ['rel/**/*.js']
-		},
+    // Add vendor prefixed styles
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer-core')({browsers: ['last 1 version']})
+        ]
+      },
+      server: {
+        options: {
+          map: true
+        },
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
 
-		includeFiles: {
-		  options: {
-		    basePath: '',
-		    baseUrl: 'src',
-		    templates: {
-		      html: {
-		        js: '<script src="{filePath}"></script>',
-		        css: '<link rel="stylesheet" type="text/css" href="{filePath}" />'
-		    	}
-		    }
-		  },
-		  myTarget: {
-		    files: {
-		      'index.html': 'src/index.html'
-		    }
-		  }
-		}
+    // Automatically inject Bower components into the app
+    wiredep: {
+      app: {
+        src: ['<%= apis.app %>/index.html'],
+        ignorePath:  /\.\.\//
+      },
+      test: {
+        devDependencies: true,
+        src: ['<%= apis.app %>/index.html'],
+        ignorePath:  /\.\.\//,
+        fileTypes:{
+          js: {
+            block: /(([\s\t]*)\/{2}\s*?bower:\s*?(\S*))(\n|\r|.)*?(\/{2}\s*endbower)/gi,
+              detect: {
+                js: /'(.*\.js)'/gi
+              },
+              replace: {
+                js: '\'{{filePath}}\','
+              }
+            }
+          }
+      }
+    }, 
 
-	});
+    // Renames files for browser caching purposes
+    filerev: {
+      dist: {
+        src: [
+          '<%= apis.dist %>/**/*.js',
+          '<%= apis.dist %>/**/*.css',
+          '<%= apis.dist %>/**/*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= apis.dist %>/styles/fonts/*'
+        ]
+      }
+    },
 
-	grunt.registerTask('miniall','minify All src/ folder', [
-		'uglify', 
-		'cssmin', 
-		'htmlmin', 
-		'imagemin'
-	]);
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      html: '<%= apis.app %>/index.html',
+      options: {
+        dest: '<%= apis.dist %>',
+        flow: {
+          html: {
+            steps: {
+              js: ['concat', 'uglifyjs'],
+              css: ['cssmin']
+            },
+            post: {}
+          }
+        }
+      }
+    },
 
-	grunt.registerTask('test','serve the files', [
-		'copy:htm',
-		'wiredep',
-		'connect:test',
-		'watch'
-	]);
+    // Performs rewrites based on filerev and the useminPrepare configuration
+    usemin: {
+      html: ['<%= apis.dist %>/**/*.html'],
+      css: ['<%= apis.dist %>/styles/**/*.css'],
+      js: ['<%= apis.dist %>/**/*.js'],
+      options: {
+        assetsDirs: [
+          '<%= apis.dist %>',
+          '<%= apis.dist %>/imgs',
+          '<%= apis.dist %>/styles'
+        ],
+        patterns: {
+          js: [[/(imgs\/[^''""]*\.(png|jpg|jpeg|gif|webp|svg))/g, 'Replacing references to imgs']]
+        }
+      }
+    },
+
+    // The following *-min tasks will produce minified files in the dist folder
+    // By default, your `index.html`'s <!-- Usemin block --> will take care of
+    // minification. These next options are pre-configured if you do not wish
+    // to use the Usemin blocks.
+    // cssmin: {
+    //   dist: {
+    //     files: {
+    //       '<%= apis.dist %>/styles/main.css': [
+    //         '.tmp/styles/{,*/}*.css'
+    //       ]
+    //     }
+    //   }
+    // },
+    // uglify: {
+    //   dist: {
+    //     files: {
+    //       '<%= apis.dist %>/scripts/scripts.js': [
+    //         '<%= apis.dist %>/scripts/scripts.js'
+    //       ]
+    //     }
+    //   }
+    // },
+    // concat: {
+    //   dist: {}
+    // },
+
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= apis.app %>/imgs',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          dest: '<%= apis.dist %>/imgs'
+        }]
+      }
+    },
+
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= apis.app %>/imgs',
+          src: '{,*/}*.svg',
+          dest: '<%= apis.dist %>/imgs'
+        }]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          collapseBooleanAttributes: true,
+          removeCommentsFromCDATA: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= apis.dist %>',
+          src: ['*.html'],
+          dest: '<%= apis.dist %>'
+        }]
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= apis.app %>',
+          dest: '<%= apis.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '*.html',
+            'imgs/**/*.{webp}',
+            'styles/fonts/**/*.*'
+          ]
+        }, {
+          expand: true,
+          cwd: '.tmp/imgs',
+          dest: '<%= apis.dist %>/imgs',
+          src: ['generated/*']
+        }]
+      },
+      styles: {
+        expand: true,
+        cwd: '<%= apis.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    // Run some tasks in parallel to speed up the build process
+    concurrent: {
+      server: [
+        'copy:styles'
+      ],
+      test: [
+        'copy:styles'
+      ],
+      dist: [
+        'copy:styles',
+        'imagemin',
+        'svgmin'
+      ]
+    }
+  });
+
+
+  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'concurrent:server',
+      'postcss:server',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve:' + target]);
+  });
+
+  grunt.registerTask('test', [
+    'clean:server',
+    'wiredep',
+    'concurrent:test',
+    'postcss',
+    'connect:test'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'wiredep',
+    'useminPrepare',
+    'concurrent:dist',
+    'postcss',
+    'concat',
+    'copy:dist',
+    'cssmin',
+    'uglify',
+    'filerev',
+    'usemin',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'newer:jscs',
+    'test',
+    'build'
+  ]);
 };
